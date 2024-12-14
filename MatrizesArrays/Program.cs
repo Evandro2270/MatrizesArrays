32083432a2e04d09a2f0cb278782e257253e1985
Evandro2270 <evandro535@gmail.com>
2024-12-14 16:58:41 -0300

    Adicionar arquivos de projeto.

---
+++ MatrizesArrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                string[,] nome = new string[3, 3];
+
+                for (int linha = 0; linha < 3; linha++)
+                {
+                    for (int coluna = 0; coluna < 3; coluna++)
+                    {
+                        nome[linha, coluna] = Console.ReadLine();
+                    }
+                }
+                
+                Console.WriteLine();
+                for (int linha = 0; linha < 3; linha++)
+                {
+                    for (int coluna = 0; coluna < 3; coluna++)
+                    {
+                        Console.Write(nome[linha, coluna] + " "); // "Espaço em branco" Concatenação
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
